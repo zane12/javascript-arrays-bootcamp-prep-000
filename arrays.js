464a6c179daef92a3839c5b3080c623f00a12c79
Zane Armstrong <zane.armstrong@comcast.net>
2018-05-29 09:05:26 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -40,3 +40,9 @@ function destructivelyRemoveElementFromBeginningOfArray(array){
 function removeElementFromBeginningOfArray(array){
   return array.slice(1)
 }
+
+function destructivelyRemoveElementFromEndOfArray(array){
+  array.pop()
+  return array
+}
+
