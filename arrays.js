c011e43f7751f62aab0ba448ba433ca6c6861a02
Zane Armstrong <zane.armstrong@comcast.net>
2018-05-29 08:36:24 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,11 +1,13 @@
 var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles']
 
+
+
 function addElementToBeginningOfArray(array, element){
-  array.push(element)
+  array = array.push(element)
   return array
 }
 
 function destructivelyAddElementToBeginningOfArray(array, element){
-  array[0] = element
-  return array
+  
+  return array.push(element)
 }
